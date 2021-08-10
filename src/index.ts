3d0667c60461d7bd4d6c65be7e618c97c99b07ac
Hiroki Osame <hiroki.osame@gmail.com>
2021-08-10 15:51:40 -0400

    feat: added omitSourcesNotFound option

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import assert from 'assert';
 import { RawSource } from 'webpack-sources';
-import AggregateError from 'aggregate-error';
-import type { Plugin } from 'webpack';
-import {
-	Options, Path, Export, Compiler, Compilation,
-} from './types';
+import type { Plugin, Compiler, compilation as c } from 'webpack';
+import WebpackError from 'webpack/lib/WebpackError.js';
+import { Options, Path, Export } from './types';
+
+type Compilation = c.Compilation;
+
+const assert = (
+	condition: any,
+	message: string,
+) => {
+	if (!condition) {
+		throw new Error(`[${EntryFilePlugin.name}] ${message}`);
+	}
+};
 
 const hasEmitAsset = (compilation: Compilation): compilation is Compilation & {
 	emitAsset: (name: string, source: RawSource) => void;
@@ -33,25 +41,56 @@ const emitAsset = (
 	}
 };
 
+const isPathPattern = /^\.\.?\//;
+
+const hasAsset = (
+	compilation: Compilation,
+	importFrom: string,
+	omitSourcesNotFound: boolean,
+) => {
+	if (!isPathPattern.test(importFrom)) {
+		return true; // ignore bare specifiers
+	}
+
+	if (
+		// eslint-disable-next-line no-prototype-builtins
+		compilation.assets.hasOwnProperty(importFrom.replace(isPathPattern, ''))
+	) {
+		return true;
+	}
+
+	if (omitSourcesNotFound) {
+		return false;
+	}
+
+	compilation.warnings.push(
+		new WebpackError(`[${EntryFilePlugin.name}] Could not resolve path "${importFrom}"`),
+	);
+	return true;
+};
+
 class EntryFilePlugin implements Plugin {
 	filename: string;
+
+	omitSourcesNotFound: boolean;
 
 	imports?: Path[];
 
 	exports?: Export[];
 
 	constructor(options: Options) {
-		const parsed = Options.safeParse(options);
+		assert(options, 'Options must be passed in');
 
-		if (!parsed.success) {
-			const { _errors } = parsed.error.format();
-			throw new AggregateError(_errors);
-		}
+		this.filename = options.filename ?? 'index.js';
+		this.omitSourcesNotFound = options.omitSourcesNotFound ?? false;
 
-		const { data } = parsed;
-		this.filename = data.filename;
-		this.imports = data.imports;
-		this.exports = data.exports;
+		assert(
+			options.imports?.length || options.exports?.length,
+			'Options must specify `imports` and/or `exports`',
+		);
+
+		this.imports = options.imports;
+		this.exports = options.exports;
 	}
 
 	apply(compiler: Compiler) {
@@ -65,12 +104,19 @@ class EntryFilePlugin implements Plugin {
 		});
 	}
 
-	createEntry() {
-		const { imports, exports } = this;
+	createEntry(compilation: Compilation) {
+		const {
+			imports,
+			exports,
+			omitSourcesNotFound,
+		} = this;
 		let index = '';
 
 		if (imports) {
 			for (const importFrom of imports) {
+				if (!hasAsset(compilation, importFrom, omitSourcesNotFound)) {
+					continue;
+				}
 				index += `import "${importFrom}";`;
 			}
 		}
@@ -78,9 +124,16 @@ class EntryFilePlugin implements Plugin {
 		if (exports) {
 			for (const exportData of exports) {
 				if (typeof exportData === 'string') {
+					if (!hasAsset(compilation, exportData, omitSourcesNotFound)) {
+						continue;
+					}
+
 					index += `export * from "${exportData}";`;
 				} else {
 					const { from, specifiers } = exportData;
+					if (!hasAsset(compilation, from, omitSourcesNotFound)) {
+						continue;
+					}
 
 					let exportValues = '*';
 
@@ -108,7 +161,7 @@ class EntryFilePlugin implements Plugin {
 	}
 
 	onCompilation(compilation: Compilation) {
-		const source = this.createEntry();
+		const source = this.createEntry(compilation);
 		emitAsset(compilation, this.filename, source);
 	}
 }
--- src/types.ts
+++ src/types.ts
@@ -1,52 +1,20 @@
-import type webpack from 'webpack';
-import { z } from 'zod';
+type Identifier = string;
 
-export type Compiler = webpack.Compiler;
-export type Compilation = webpack.compilation.Compilation;
+type Specifier = Identifier | {
+	name: Identifier;
+	as?: Identifier;
+};
 
-const SpecifierString = z.string().min(1, {
-	message: 'Specifier is required',
-});
-const Specifier = z.union([
-	SpecifierString,
-	z.object({
-		name: SpecifierString,
-		as: SpecifierString.optional(),
-	}),
-]);
+export type Path = string;
 
-export const Path = z.string().min(1, {
-	message: 'Path is required',
-}).refine(
-	path => !path.startsWith('/'),
-	{
-		message: 'Path can only be relative or a bare specifier',
-	},
-);
+export type Export = Path | {
+	from: Path;
+	specifiers?: Specifier[];
+};
 
-export type Path = z.infer<typeof Path>;
-
-export const Export = z.union([
-	Path,
-	z.object({
-		from: Path,
-		specifiers: z.array(Specifier).nonempty({
-			message: 'Specifiers array empty',
-		}).optional(),
-	}),
-]);
-
-export type Export = z.infer<typeof Export>;
-
-export const Options = z.object({
-	filename: z.string().min(1).default('index.js'),
-	imports: z.array(Path).nonempty().optional(),
-	exports: z.array(Export).nonempty().optional(),
-}).refine(
-	options => options.imports?.length || options.exports?.length,
-	{
-		message: 'Options must specify `imports` and/or `exports` array',
-	},
-);
-
-export type Options = z.infer<typeof Options>;
+export type Options = {
+	filename?: string;
+	imports?: Path[];
+	exports?: Export[];
+	omitSourcesNotFound?: boolean;
+};
